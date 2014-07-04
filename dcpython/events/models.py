74d0a464db9f39c3f171f1dc3c9f30e6a439a2fa
David Greisen <dgreisen@gmail.com>
2014-07-04 12:43:26 -0400

    fixed meetup venue import

--- dcpython/events/models.py
+++ dcpython/events/models.py
@@ -24,23 +24,28 @@ class Venue(models.Model):
     address_1 = models.CharField(max_length=200)
     address_2 = models.CharField(max_length=200, blank=True)
     address_3 = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=15, default='', blank=True)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=2)
     zip_code = models.CharField(max_length=5, blank=True)
-
     repinned = models.BooleanField()
 
     @classmethod
     def create_from_meetup(cls, meetup_data):
-        i, created = cls.objects.get_or_create(meetup_id=meetup_data['id'])
+        meetup_data['meetup_id'] = meetup_data.pop('id')
+        meetup_data['zip_code'] = meetup_data.pop('zip', '')
+        print meetup_data
+        try:
+            venue = cls.objects.get(meetup_id=meetup_data['meetup_id'])
+        except cls.DoesNotExist:
+            venue = cls(**meetup_data)
+        else:
+            for k, v in meetup_data.items():
+                setattr(venue, k, v)
 
-        for k, v in meetup_data.items():
-            if k == "id": continue
-            setattr(i, k, v)
-
-        i.full_clean()
-        i.save()
-        return i
+        venue.full_clean()
+        venue.save()
+        return venue
 
 
 class EventManager(models.Manager):
