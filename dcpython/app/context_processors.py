b121eeb8754ee0cc10c456c42297f596f7d18d63
David Greisen <dgreisen@gmail.com>
2014-07-06 22:18:12 -0400

    google analytics works

--- dcpython/app/context_processors.py
+++ dcpython/app/context_processors.py
@@ -1,6 +1,10 @@
 # encoding: utf-8
-
+from django.conf import settings
 
 def path_hierarchy(request):
-    """Make it easy for templates to access URL path components"""
+    """ Make it easy for templates to access URL path components """
     return {'PATH_HIERARCHY': filter(None, request.path.strip('/').split('/')) }
+
+def google_analytics(request):
+	""" Make it easy for templates to access google analytics key """
+	return {'GOOGLE_ANALYTICS_ID': settings.GOOGLE_ANALYTICS_ID}
