8d172b1786f793640a911c995abb196022fee158
Davis Silverman <SilvermD@advisory.com>
2014-07-23 16:22:16 -0400

    Added URL which didnt get commmitted, added newline

--- dcpython/blog/urls.py
+++ dcpython/blog/urls.py
@@ -17,4 +17,5 @@ urlpatterns = patterns('dcpython.blog.views',
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[^/]+)/$',
         PostDetail.as_view(),
         name="post-detail"),
+    url(r'^feed/$', BlogFeed())
 )
