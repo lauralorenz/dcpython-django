5a6c6b8cf113bce358b318898b32996f96184a76
David Greisen <dgreisen@gmail.com>
2014-07-31 23:35:16 -0400

    Added youtube sync support
    Youtube now syncs through a management command. links playlists
    via a meetup url in the video summary. user rss feed. url stored
    in settings.py
    
    removed old method of looking up videos at runtime
    
    added service sync model to app to keep track of last successful
    service sync.

--- dcpython/app/models.py
+++ dcpython/app/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     meetup_username = models.CharField(max_length=100, blank=True, null=True)
     meetup_id = models.IntegerField(blank=True, null=True)
+
+class ServiceSync(models.Model):
+    """
+    keep track of the last time a sync event took place
+    """
+    service = models.CharField(max_length=200)
+    last_synced = models.CharField(max_length=50)
